d167e2b13b676936c1d1822a004f6c0bc4d9149c
MatiasRoje <rojechi@gmail.com>
2023-09-29 11:36:54 +0200

    300 Using Flexbox

--- src/features/user/Username.tsx
+++ src/features/user/Username.tsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Alice</div>;
+  return <div className="hidden text-sm font-semibold md:block">Alice</div>;
 }
 
 export default Username;
